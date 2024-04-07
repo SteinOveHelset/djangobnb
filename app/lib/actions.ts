4f9e33c2346d9065b5c0583e49c1e40040eec87c
Stein Ove Helset <steinovehelset@gmail.com>
2024-04-07 20:26:59 +0200

    fixed scroll error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,7 +24,7 @@ export async function handleRefresh() {
             if (json.access) {
                 cookies().set('session_access_token', json.access, {
                     httpOnly: true,
-                    secure: process.env.NODE_ENV === 'production',
+                    secure: false,
                     maxAge: 60 * 60, // 60 minutes
                     path: '/'
                 });
@@ -46,21 +46,21 @@ export async function handleRefresh() {
 export async function handleLogin(userId: string, accessToken: string, refreshToken: string) {
     cookies().set('session_userid', userId, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: false,
         maxAge: 60 * 60 * 24 * 7, // One week
         path: '/'
     });
 
     cookies().set('session_access_token', accessToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: false,
         maxAge: 60 * 60, // 60 minutes
         path: '/'
     });
 
     cookies().set('session_refresh_token', refreshToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: false,
         maxAge: 60 * 60 * 24 * 7, // One week
         path: '/'
     });
